62aca4cdf9a61a9f64e4942a493286aa48da1dfd
Mossakowski Kamil <mosakamil@gmail.com>
2018-11-22 21:30:17 +0100

    add remove onu

--- main.js
+++ main.js
@@ -2,12 +2,15 @@
 
     'use strict';
 
+    const arrayEvents = ['click', 'keyup'];
+    
     function addAnimation() {
-
+        
         var patrickAnimate = document.getElementsByClassName('patrick');
         var naszMistrzImg = document.getElementById('nasz-mistrz');
 
         for (let i = 0; i < patrickAnimate.length; i++) {
+            console.log(i);
             patrickAnimate[i].addEventListener('click', function () {
                 naszMistrzImg.classList.add('shake');
                 setTimeout(function () {
@@ -21,23 +24,35 @@
 
         console.log(getCardPon);
         var setLocalizationOnu = document.getElementsByClassName('setLocalizationOnu');
-        var tab = ['click', 'keyup'];
         for (let i = 0; i < setLocalizationOnu.length; i++) {
-            for (let j = 0; j < tab.length; j++) {
-                setLocalizationOnu[i].addEventListener(tab[j], function () {
+            for (let j = 0; j < arrayEvents.length; j++) {
+                setLocalizationOnu[i].addEventListener(arrayEvents[j], function () {
                     let getCardPon = document.getElementById('getCardPon').elements;
                     let card_number = getCardPon[0].value;
                     let pon_number = getCardPon[1].value;
-                    var commend = 'show onu all-status epon-olt_0/' + card_number + '/' + pon_number;
-                    var btn = document.getElementById('button-search-free-space');
+                    let commend = 'show onu all-status epon-olt_0/' + card_number + '/' + pon_number;
+                    let btn = document.getElementById('button-search-free-space');
                     btn.setAttribute('data-clipboard-text', commend);
 
                 });
             }
         }
-
+    }
+    
+    function removeOnu() {
+        var btn = document.getElementById('button-remove-onu');
+        var form = document.getElementById('deleteOnu').elements;
+        for(let i=0; i < arrayEvents.length; i++) {
+            form[0].addEventListener(arrayEvents[i], function() {
+               let onuId = form[0].value;
+                let commend = 'no onu ' + onuId;
+                btn.setAttribute('data-clipboard-text',commend);
+            });
+        }
+        
     }
 
     addAnimation();
     updateOltPon();
+    removeOnu();
 })();
