2218dcfac3717330633a7be4233dbab635327f96
Mossakowski Kamil <mosakamil@gmail.com>
2018-11-22 23:31:49 +0100

    new function - add onu

--- main.js
+++ main.js
@@ -2,15 +2,14 @@
 
     'use strict';
 
-    const arrayEvents = ['click', 'keyup'];
-    
+    const arrayEvents = ['click', 'keyup', 'mouseleave'];
+
     function addAnimation() {
-        
+
         var patrickAnimate = document.getElementsByClassName('patrick');
         var naszMistrzImg = document.getElementById('nasz-mistrz');
 
         for (let i = 0; i < patrickAnimate.length; i++) {
-            console.log(i);
             patrickAnimate[i].addEventListener('click', function () {
                 naszMistrzImg.classList.add('shake');
                 setTimeout(function () {
@@ -21,8 +20,6 @@
     }
 
     function updateOltPon() {
-
-        console.log(getCardPon);
         var setLocalizationOnu = document.getElementsByClassName('setLocalizationOnu');
         for (let i = 0; i < setLocalizationOnu.length; i++) {
             for (let j = 0; j < arrayEvents.length; j++) {
@@ -38,21 +35,57 @@
             }
         }
     }
-    
+
     function removeOnu() {
         var btn = document.getElementById('button-remove-onu');
-        var form = document.getElementById('deleteOnu').elements;
-        for(let i=0; i < arrayEvents.length; i++) {
-            form[0].addEventListener(arrayEvents[i], function() {
-               let onuId = form[0].value;
+        var form = document.getElementById('form-delete-onu').elements;
+        for (let i = 0; i < arrayEvents.length; i++) {
+            form[0].addEventListener(arrayEvents[i], function () {
+                let onuId = form[0].value;
                 let commend = 'no onu ' + onuId;
-                btn.setAttribute('data-clipboard-text',commend);
+                btn.setAttribute('data-clipboard-text', commend);
             });
         }
-        
+    }
+
+    function addOnu() {
+        var btn = document.getElementById('button-add-onu');
+        var form = document.getElementById('form-add-onu').elements;
+
+        //get onu number and update commend
+        for (let i = 0; i < arrayEvents.length; i++) {
+            form[0].addEventListener(arrayEvents[i], function () {
+                let onuId = form[0].value;
+                let commend = btn.getAttribute('data-clipboard-text').split(' ');
+                commend[1] = onuId;
+                btn.setAttribute('data-clipboard-text', commend.join(' '));
+
+            });
+        }
+
+        //get onu type and update commend
+        form[1].addEventListener('click', function () {
+            let onuType = form[1].value;
+            let commend = btn.getAttribute('data-clipboard-text').split(' ');
+            commend[3] = onuType;
+            btn.setAttribute('data-clipboard-text', commend.join(' '));
+        });
+
+        console.log(form);
+
+        //get onu mac and update commend
+        for (let i = 0; i < arrayEvents.length; i++) {
+            form[2].addEventListener(arrayEvents[i], function () {
+                let onuMac = this.value;
+                let commend = btn.getAttribute('data-clipboard-text').split(' ');
+                commend[5] = onuMac;
+                btn.setAttribute('data-clipboard-text', commend.join(' '));
+            });
+        }
     }
 
     addAnimation();
     updateOltPon();
     removeOnu();
+    addOnu();
 })();
